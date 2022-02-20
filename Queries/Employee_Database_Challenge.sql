c31277d2d947ef29444caf7755506463b50ca0e4
stovepipe <zach.mehlenbacher@gmail.com>
2022-02-20 08:53:43 -0500

    Deliverable 1 and 2 Complete

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,10 +1,65 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-- Deliverable 1
+-- Gather all employees retiring with titles
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+-- Review table before export
+SELECT * FROM retirement_titles;
+
+-- Selecting the most recent title of employees approaching retirement
+SELECT DISTINCT ON (rt.emp_no)
+	rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE rt.to_date = ('9999-01-01')
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+-- Review table before export
+SELECT * FROM unique_titles;
+
+-- Count of how many employees of each title are retiring
+SELECT COUNT(ut.title),
+ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.title) DESC;
+
+-- Review table before export
+SELECT * FROM retiring_titles;
+
+-- Deliverable 2
+-- Determining employees eligible for mentorship program
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE de.to_date = ('9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no, de.to_date DESC;
+
+-- Review table before export
+SELECT * FROM mentorship_eligibility;
